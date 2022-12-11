a7da151a03a35adc0adb8fd1a2d0a525e4cb4e72
seunghyeon moon <74526652+max38744@users.noreply.github.com>
2022-12-11 11:54:57 +0900

    Revert "merge를 위한 pr"

--- test.py
+++ test.py
@@ -6,31 +6,47 @@ from pyspark.sql.types import StructField, StructType, StringType, LongType, Row
 if __name__ =="__main__" :
 
 	myManualSchema = StructType([
-		StructField("news_id", StringType(), True),
-		StructField("title", StringType(), True),
-		StructField("timestamp", StringType(), True),
-		StructField("content", StringType(), True),
+		StructField("a", StringType(), True),
+		StructField("b", StringType(), True),
+		StructField("c", StringType(), True),
+		StructField("d", StringType(), True),
+		StructField("e", StringType(), True),
+		StructField("f", StringType(), True),
+		StructField("g", StringType(), True),
+		StructField("h", StringType(), True),
+		StructField("i", StringType(), True)
+		#StructField("g", StringType(), True),
+		#StructField("k", StringType(), True),
+		#StructField("l", StringType(), True),
+		#StructField("m", StringType(), True),
+		#StructField("n", StringType(), True),
+		#StructField("o", StringType(), True),
+		#StructField("p", StringType(), True),
+		#StructField("q", StringType(), True),
+		#StructField("r", StringType(), True)
 	])
 
 	spark = SparkSession.builder.appName("test").getOrCreate()
-	df_news = spark.read.schema(myManualSchema).load("hdfs:///user/maria_dev/dataset.csv",\
-			format="csv", sep = ",", inferSchema = "true", header = "true")	
-	df_news = df_news.filter("content is not null")
+	df_news = spark.read.schema(myManualSchema).load("hdfs:///user/maria_dev/NewsResult_20220826-20221126.csv",\
+			format="csv", sep = ",", inferSchema = "true", header = "true", charset = "euc-kr")	
+	df_news = df_news.filter("c is not null")
 	df = df_news\
 			.rdd\
-			.map(lambda row: Row(news_id=row.news_id, title=row.title, timestamp = row.timestamp, content=Okt().nouns(row.content)))\
+			.map(lambda row: Row(title=row.a, content=row.b, text=Okt().nouns(row.c)))\
 			.toDF()
-	df.show()
 
+
+	okt = Okt()
+	def tokenizer(x) :
+		return okt.nouns(x)
+
+	udf_token = F.udf(tokenizer)
 	
-
-
-
-	
-#	a = df.select(
-#			udf_token(F.col('c')).alias('new_c'),
-#			F.col('a')
-#	)
+	a = df.select(
+			udf_token(F.col('c')).alias('new_c'),
+			F.col('a')
+	)
 	#df.withColumn('new', F.udf(lambda x : okt.nouns(x))( "본문")).show()
+	a.show()
 
 	
